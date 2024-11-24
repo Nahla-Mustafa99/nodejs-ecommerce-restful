5a9b3dbf45c87e13678e495116567fc98bf8c63c
Nahla-Mustafa99 <nahla0019@gmail.com>
2024-11-24 18:14:18 +0200

    feat(): add validation layer to category service

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { check, param, validationResult } = require("express-validator");
 
 const {
   getCategories,
@@ -8,12 +9,70 @@ const {
   updateCategory,
   deleteCategory,
 } = require("../services/categoryService");
+const ApiError = require("../utils/apiError");
 
-router.route("/").get(getCategories).post(createCategory);
+router
+  .route("/")
+  .get(getCategories)
+  .post(
+    check("name")
+      .notEmpty()
+      .withMessage("Category name is required")
+      .isLength({ min: 3 })
+      .withMessage("Too short category name")
+      .isLength({ max: 32 })
+      .withMessage("Too long category name"),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      return next();
+    },
+    createCategory
+  );
+
 router
   .route("/:id")
-  .get(getCategory)
-  .put(updateCategory)
-  .delete(deleteCategory);
+  .get(
+    param("id").isMongoId().withMessage("Invalid category id format"),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      return next();
+    },
+    getCategory
+  )
+  .put(
+    check("id").isMongoId().withMessage("Invalid category id format"),
+    check("name")
+      .notEmpty()
+      .withMessage("Category name is required")
+      .isLength({ min: 3 })
+      .withMessage("Too short category name")
+      .isLength({ max: 32 })
+      .withMessage("Too long category name"),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      return next();
+    },
+    updateCategory
+  )
+  .delete(
+    check("id").isMongoId().withMessage("Invalid category id format"),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      return next();
+    },
+    deleteCategory
+  );
 
 module.exports = router;
