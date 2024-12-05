bee79bbbaa83bba1ae8fa523555f3ceb292657cb
Nahla-Mustafa99 <nahla0019@gmail.com>
2024-12-05 16:47:53 +0200

    chore(productService): add some features to get products handler
    add pagination, filteration, sorting, field limiting, and searching features to get products handler

--- services/productService.js
+++ services/productService.js
@@ -19,11 +19,57 @@ exports.createProduct = asyncHandler(async (req, res, next) => {
 // @route GET /api/v1/products
 // @access Public
 exports.getProducts = asyncHandler(async (req, res, next) => {
+  // 1- Pagination
   const page = +req.query.page || 1;
-  const resultsPerPage = +req.query.limit || 5;
-  const skip = (page - 1) * resultsPerPage;
+  // limit = results per page
+  const limit = +req.query.limit || 5;
+  const skip = (page - 1) * limit;
 
-  const products = await Product.find().skip(skip).limit(resultsPerPage);
+  // 2- Filteration
+  const queryStringObj = { ...req.query };
+  // not excluded things make faulty results
+  const excludedFields = ["page", "limit", "fields", "sort", "keyword"];
+  excludedFields.forEach((field) => delete queryStringObj[field]);
+  const queryStr = JSON.stringify(queryStringObj).replace(
+    /\b(gte|gt|lte|lt)\b/g,
+    (match) => `$${match}`
+  );
+  const queryObj = JSON.parse(queryStr);
+
+  // Build query
+  const mongooseQuery = Product.find(queryObj).skip(skip).limit(limit);
+
+  // 3- Sorting
+  if (req.query.sort) {
+    // -price, sold -> "price sold"
+    // req.quey.sort is string not like filter object
+    const sortBy = req.query.sort.split(",").join(" ");
+    mongooseQuery.sort(sortBy);
+  } else {
+    mongooseQuery.sort("-createdAt");
+  }
+
+  // 4- Field Limiting
+  if (req.query.fields) {
+    const fields = req.query.fields.split(",").join(" ");
+    mongooseQuery.select(fields);
+  } else {
+    mongooseQuery.select("-__v");
+  }
+
+  // 5- Search
+  if (req.query.keyword) {
+    const keword = req.query.keyword;
+    const searchQuery = {};
+    searchQuery.$or = [
+      { title: { $regex: keword, $options: "i" } },
+      { description: { $regex: keword, $options: "i" } },
+    ];
+    mongooseQuery.find(searchQuery);
+  }
+
+  // Execute query
+  const products = await mongooseQuery;
 
   return res
     .status(200)
@@ -47,7 +93,8 @@ exports.getProduct = asyncHandler(async (req, res, next) => {
 // @access Private
 exports.updateProduct = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
-  req.body.slug = slugify(req.body.title);
+  const title = req.body?.title;
+  if (title) req.body.slug = slugify(title);
 
   const product = await Product.findByIdAndUpdate({ _id: id }, req.body, {
     new: true,
