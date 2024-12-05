919a7b226508c8a81dcab3e94e7dbc82096b222f
Nahla-Mustafa99 <nahla0019@gmail.com>
2024-12-05 20:50:19 +0200

    refactor: refactor api features

--- services/productService.js
+++ services/productService.js
@@ -3,6 +3,7 @@ const slugify = require("slugify");
 
 const ApiError = require("../utils/apiError");
 const Product = require("../models/productModel");
+const ApiFeatures = require("../utils/apiFeatures");
 
 // @desc create product
 // @route POST /api/v1/products
@@ -19,61 +20,26 @@ exports.createProduct = asyncHandler(async (req, res, next) => {
 // @route GET /api/v1/products
 // @access Public
 exports.getProducts = asyncHandler(async (req, res, next) => {
-  // 1- Pagination
-  const page = +req.query.page || 1;
-  // limit = results per page
-  const limit = +req.query.limit || 5;
-  const skip = (page - 1) * limit;
-
-  // 2- Filteration
-  const queryStringObj = { ...req.query };
-  // not excluded things make faulty results
-  const excludedFields = ["page", "limit", "fields", "sort", "keyword"];
-  excludedFields.forEach((field) => delete queryStringObj[field]);
-  const queryStr = JSON.stringify(queryStringObj).replace(
-    /\b(gte|gt|lte|lt)\b/g,
-    (match) => `$${match}`
-  );
-  const queryObj = JSON.parse(queryStr);
-
-  // Build query
-  const mongooseQuery = Product.find(queryObj).skip(skip).limit(limit);
-
-  // 3- Sorting
-  if (req.query.sort) {
-    // -price, sold -> "price sold"
-    // req.quey.sort is string not like filter object
-    const sortBy = req.query.sort.split(",").join(" ");
-    mongooseQuery.sort(sortBy);
-  } else {
-    mongooseQuery.sort("-createdAt");
-  }
-
-  // 4- Field Limiting
-  if (req.query.fields) {
-    const fields = req.query.fields.split(",").join(" ");
-    mongooseQuery.select(fields);
-  } else {
-    mongooseQuery.select("-__v");
-  }
-
-  // 5- Search
-  if (req.query.keyword) {
-    const keword = req.query.keyword;
-    const searchQuery = {};
-    searchQuery.$or = [
-      { title: { $regex: keword, $options: "i" } },
-      { description: { $regex: keword, $options: "i" } },
-    ];
-    mongooseQuery.find(searchQuery);
-  }
+  const apiFeatures = new ApiFeatures(Product.find(), req.query)
+    .search("Product")
+    .filter();
+  const documentsCount = await apiFeatures.mongooseQuery.countDocuments();
+  // const documentsCount = await Product.countDocuments();
+  apiFeatures.mongooseQuery = Product.find();
+  apiFeatures
+    .sort()
+    .fieldLimit()
+    .paginate(documentsCount)
+    .search("Product")
+    .filter();
 
   // Execute query
+  const { mongooseQuery, paginationResult } = apiFeatures;
   const products = await mongooseQuery;
 
   return res
     .status(200)
-    .json({ results: products.length, page: page, data: products });
+    .json({ results: products.length, paginationResult, data: products });
 });
 
 // @desc get a specific product
---
+++ utils/apiFeatures.js
@@ -0,0 +1,106 @@
+const { countDocuments } = require("../models/productModel");
+
+class ApiFeatures {
+  constructor(mongooseQuery, queryObj) {
+    this.mongooseQuery = mongooseQuery;
+    this.queryObj = queryObj;
+    this.paginationResult = {};
+  }
+
+  // Filteration
+  filter() {
+    const queryStringObj = { ...this.queryObj };
+    // not excluded things make faulty results
+    const excludedFields = ["page", "limit", "fields", "sort", "keyword"];
+    excludedFields.forEach((field) => delete queryStringObj[field]);
+    // Apply filtration using [gte, gt, lte, lt]
+    let queryStr = JSON.stringify(queryStringObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    const filterQueryObj = JSON.parse(queryStr);
+
+    // Update mongooseQuery
+    this.mongooseQuery = this.mongooseQuery.find(filterQueryObj);
+
+    return this;
+  }
+
+  // Sorting
+  sort() {
+    if (this.queryObj.sort) {
+      // -price, sold -> "price sold"
+      // req.quey.sort is string not like filter object
+      const sortBy = this.queryObj.sort.split(",").join(" ");
+      this.mongooseQuery = this.mongooseQuery.sort(sortBy);
+    } else {
+      this.mongooseQuery = this.mongooseQuery.sort("-createdAt");
+    }
+    return this;
+  }
+
+  // Field Limiting
+  fieldLimit() {
+    if (this.queryObj.fields) {
+      const fields = this.queryObj.fields.split(",").join(" ");
+      this.mongooseQuery = this.mongooseQuery.select(fields);
+    } else {
+      this.mongooseQuery = this.mongooseQuery.select("-__v");
+    }
+    return this;
+  }
+
+  // Search
+  search(modelName) {
+    if (this.queryObj.keyword) {
+      const keword = this.queryObj.keyword;
+      const searchQuery = {};
+      if (modelName === "Product") {
+        searchQuery.$or = [
+          { title: { $regex: keword, $options: "i" } },
+          { description: { $regex: keword, $options: "i" } },
+        ];
+      } else {
+        searchQuery.$or = [{ name: { $regex: keword, $options: "i" } }];
+      }
+      this.mongooseQuery = this.mongooseQuery.find(searchQuery);
+    }
+    // console.log(this.mongooseQuery.countDocuments());
+    return this;
+  }
+
+  // Pagination
+  paginate(documentsCount) {
+    const page = +this.queryObj.page || 1;
+    const limit = +this.queryObj.limit || 10; // limit = results per page
+    const skip = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    this.paginationResult.currentPage = page;
+    this.paginationResult.limit = limit;
+    this.paginationResult.numberOfPages = Math.ceil(documentsCount / limit);
+    // next page
+    if (endIndex < documentsCount) {
+      this.paginationResult.next = page + 1;
+    }
+    // Previous page
+    if (page !== 1) {
+      this.paginationResult.prev = page - 1;
+    }
+    this.mongooseQuery = this.mongooseQuery.skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
+module.exports = ApiFeatures;
+
+// Update paginationResult
+//  this.mongooseQuery.countDocuments().then((count) => {
+//   this.paginationResult.numberOfPages = Math.ceil(
+//     count / this.paginationResult.limit
+//   );
+//   const endIndex =
+//     this.paginationResult.currentPage * this.paginationResult.limit;
+//   if (endIndex < count) {
+//     this.paginationResult.next = page + 1;
+//   }
+// });
