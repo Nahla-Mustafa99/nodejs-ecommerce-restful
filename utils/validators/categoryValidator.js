aa3cf6a79e3480b1e7f4ab04c4560d01892971b8
Nahla-Mustafa99 <nahla0019@gmail.com>
2025-01-18 19:25:29 +0200

    add some extra validations to categoryValidator

--- utils/validators/categoryValidator.js
+++ utils/validators/categoryValidator.js
@@ -1,7 +1,8 @@
-const { param, check } = require("express-validator");
+const { param, check, checkExact } = require("express-validator");
 const slugify = require("slugify");
 
 const validatorMiddleware = require("../../middlewares/validatorMiddleware");
+const Category = require("../../models/categoryModel");
 
 exports.getCategoryValidator = [
   param("id").isMongoId().withMessage("Invalid category id format"),
@@ -9,32 +10,73 @@ exports.getCategoryValidator = [
 ];
 
 exports.createCategoryValidator = [
-  check("name")
-    .notEmpty()
-    .withMessage("Category name is required")
-    .isLength({ min: 3 })
-    .withMessage("Too short category name")
-    .isLength({ max: 32 })
-    .withMessage("Too long category name")
-    .custom((val, { req }) => {
-      req.body.slug = slugify(val);
-      return true;
-    }),
+  checkExact(
+    [
+      check("name")
+        .notEmpty()
+        .withMessage("Category name is required")
+        .isLength({ min: 3 })
+        .withMessage("Too short category name")
+        .isLength({ max: 32 })
+        .withMessage("Too long category name")
+        .custom((val, { req }) => {
+          return Category.findOne({ name: val }).then((categoryDoc) => {
+            if (categoryDoc) {
+              return Promise.reject(
+                new Error(
+                  `There is a category with this name '${val}' already, please pick a different one.`
+                )
+              );
+            }
+            req.body.slug = slugify(val);
+            return true;
+          });
+        }),
+      check("slug"),
+      check("image").optional(),
+    ],
+    {
+      message:
+        "Too many fields specified, only ( name - image ) fields are allowed",
+    }
+  ),
   validatorMiddleware,
 ];
 
 exports.updateCategoryValidator = [
-  check("id").isMongoId().withMessage("Invalid category id format"),
-  check("name")
-    .optional()
-    .isLength({ min: 3 })
-    .withMessage("Too short category name")
-    .isLength({ max: 32 })
-    .withMessage("Too long category name")
-    .custom((val, { req }) => {
-      req.body.slug = slugify(val);
-      return true;
-    }),
+  checkExact(
+    [
+      check("id").isMongoId().withMessage("Invalid category id format"),
+      check("name")
+        .optional()
+        .isLength({ min: 3 })
+        .withMessage("Too short category name")
+        .isLength({ max: 32 })
+        .withMessage("Too long category name")
+        .custom((val, { req }) => {
+          return Category.findOne({ name: val }).then((categoryDoc) => {
+            if (
+              categoryDoc &&
+              categoryDoc._id.toString() !== req.params.id.toString()
+            ) {
+              return Promise.reject(
+                new Error(
+                  `There is a category with this name '${val}' already, please pick a different one.`
+                )
+              );
+            }
+            req.body.slug = slugify(val);
+            return true;
+          });
+        }),
+      check("slug"),
+      check("image").optional(),
+    ],
+    {
+      message:
+        "Too many fields specified, only ( name - image ) fields are allowed",
+    }
+  ),
   validatorMiddleware,
 ];
 
