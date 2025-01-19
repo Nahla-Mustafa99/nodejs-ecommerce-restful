9999b5fd398eadc6f1bfb55f9c7ea106b3353cf1
Nahla-Mustafa99 <nahla0019@gmail.com>
2025-01-19 14:56:21 +0200

    chore: setup some middlewares for production

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const path = require("path");
+const hpp = require("hpp");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const cors = require("cors");
+const compression = require("compression");
 
 dotenv.config({ path: "config.env" });
 const db_connection = require("./config/database");
 const ApiError = require("./utils/apiError");
 const globalErrorHandler = require("./middlewares/errorMiddleware");
+
 const { webhookCheckout } = require("./services/orderService");
 const mountRoutes = require("./routes");
 
@@ -15,6 +21,17 @@ db_connection();
 
 // express app
 const app = express();
+
+// Help secure the app by setting HTTP response headers.
+app.use(helmet());
+
+// Enable cors
+app.use(cors());
+// Enable cors for "pre-flight" requests
+app.use("*", cors());
+
+// compress all responses
+app.use(compression());
 
 // Checkout webhook
 app.post(
@@ -33,6 +50,32 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
   console.log(`We're on the ${process.env.NODE_ENV} mode `);
 }
+
+// Middleware to protect against HTTP Parameter Pollution attacks
+app.use(
+  hpp({
+    whitelist: [
+      "price",
+      "sold",
+      "quantity",
+      "ratingsAverage",
+      "ratingsQuantity",
+    ],
+  })
+);
+
+// Apply the rate limiting middleware to all requests
+app.use(
+  "/api",
+  rateLimit({
+    // To limit repeated requests: limit each IP to 100 requests per `window` (per 15 minutes).
+    windowMs: 15 * 60 * 1000, // 15m
+    limit: 100, // 100 requests per 15m per window
+    // response to return after limit is reached
+    message:
+      "Too many request created from this IP, please try again after an hour",
+  })
+);
 
 // Mount Routes...
 mountRoutes(app);
